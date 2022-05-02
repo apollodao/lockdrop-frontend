7389e9a0f65aa3c3b9f1280249c88e77c0868e46
othermike <othermike@apollo.farm>
2022-05-02 15:00:43 -0400

    fix: xastro/astro tokens

--- store/networks.ts
+++ store/networks.ts
@@ -32,8 +32,8 @@ export const networks: { [k in SupportedNetwork]: LocalNetworkConfig } = {
   mainnet: {
     contracts: {
       apolloLockdrop: 'terra120z72wqvrtfjgyxcdnhnxn5e5chxz7ruud290n',
-      xastro_token: 'terra1xj49zyqrwpv5k928jwfpfy2ha668nwdgkwlrg3',
-      astro_token: 'terra14lpnyzc9z4g3ugr4lhm8s4nle0tq8vcltkhzh7'
+      xastro_token: 'terra14lpnyzc9z4g3ugr4lhm8s4nle0tq8vcltkhzh7',
+      astro_token: 'terra1xj49zyqrwpv5k928jwfpfy2ha668nwdgkwlrg3'
     },
     fee: { gasPrice: 0.15, amount: 100000 }, // 0.1 UST
     stats: 'https://graphql.apollo.farm/graphql',
